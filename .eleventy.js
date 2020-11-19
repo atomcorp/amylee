0d546a0712f2c264884233a115f9479d3a00bcc4
atomcorp <thomasmaxwellsmith@gmail.com>
2020-11-19 22:32:38 +0000

    fix css and text

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const CleanCSS = require("clean-css");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.setTemplateFormats(["css", "html", "md"]);
+  eleventyConfig.setTemplateFormats(["css", "html", "md", "njk"]);
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
   return {
     passthroughFileCopy: true,
   };
